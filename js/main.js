c75e9a7abf5f285f643df699351d087f3e72b19f
phillipnzau <nzauonline@gmail.com>
2022-09-20 12:10:58 +0300

    Navigation mobile complete

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 "use strict";
 let isActive = false;
+let el = document.getElementById("toggle");
 
 function myFunc() {
   isActive = !isActive;
-  let el = document.getElementById("toggle");
 
-  return (
-    isActive
-      ? (el.classList.remove("header__menu-inactive"),
-        el.classList.add("header__menu-active"))
-      : el.classList.add("header__menu-inactive"),
-    el.classList.remove("header__menu-active")
-  );
+  return isActive
+    ? (el.classList.remove("header__menu-inactive"),
+      el.classList.add("header__menu-active"))
+    : (el.classList.add("header__menu-inactive"),
+      el.classList.remove("header__menu-active"));
 }
+
+/// Get the anchor tag and close the menu
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.p;
+    isActive = false;
+    el.classList.remove("header__menu-active");
+    el.classList.add("header__menu-inactive");
+  });
+});
